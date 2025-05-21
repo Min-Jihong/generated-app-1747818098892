39b41db9d8b364f574978f4c11299728be8d83fd
Min-Jihong <69140228+Min-Jihong@users.noreply.github.com>
2025-05-21 18:01:44 +0900

    Add src/pages/product/details.tsx

---
+++ src/pages/product/details.tsx
@@ -0,0 +1,9 @@
+export default function ProductDetails() {
+  // Here you would fetch product details using react-query or any other method
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>This is where product details will be shown.</p>
+    </div>
+  );
+}
