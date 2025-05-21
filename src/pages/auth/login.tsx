a1980b5f4262da0a3437dfb1cd65b308a58d04ea
Min-Jihong <69140228+Min-Jihong@users.noreply.github.com>
2025-05-21 18:01:42 +0900

    Add src/pages/auth/login.tsx

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '../../components/forms/login-form';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <LoginForm />
+    </div>
+  );
+}
